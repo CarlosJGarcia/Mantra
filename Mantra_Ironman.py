2cf61318a3341492d31e175f18ed7facae855fb8
CarlosJGarcia <cgarciams@hotmail.com>
2025-06-19 08:11:38 +0200

    Como ejecutar en VSCode y sincronizar Github

--- Mantra_Ironman.py
+++ Mantra_Ironman.py
@@ -1,13 +1,18 @@
 # Mantra del Ironman en Python 17/Jun/2025
 # Pulsar F5 para ejecutar el código en IDLE
+# Pulsar Ctrl+F5 para ejecutar el código en VSCode
 # En macOS, para que funcione F5, pulsar Fn (globo terráqueo) + F5
+# $ git commit -m "Comentario"
+# $ git push
 
 mantra = ["Confío en mí. He entrenado lo mejor que he podido y domino la distancia, por ello nadaré, pedalearé y correré para mí."]
 mantra.append("Los demás compiten conmigo, pero no son mis adversarios. Compito para mí.")
 mantra.append("Debo ver el triatlón como algo divertido. Alegría y diversión pese a la dificultad. Porque tengo el privilegio de deslizarme.")
+
 print("\nMantra del Ironman")
 n = 1
 for item in mantra:
     print(f"{n}: {item}")
     n += 1
 print()
+print("Fin")
