87ef9ec696a1a7a745ca3f24ae4e4c91a9438e0b
CarlosJGarcia <cgarciams@hotmail.com>
2025-06-19 08:02:50 +0200

    Initial commit.

--- Mantra_Ironman.py
+++ Mantra_Ironman.py
@@ -3,7 +3,8 @@
 # En macOS, para que funcione F5, pulsar Fn (globo terráqueo) + F5
 
 mantra = ["Confío en mí. He entrenado lo mejor que he podido y domino la distancia, por ello nadaré, pedalearé y correré para mí."]
-#mantra.append("jojo")
+mantra.append("Los demás compiten conmigo, pero no son mis adversarios. Compito para mí.")
+mantra.append("Debo ver el triatlón como algo divertido. Alegría y diversión pese a la dificultad. Porque tengo el privilegio de deslizarme.")
 print("\nMantra del Ironman")
 n = 1
 for item in mantra:
