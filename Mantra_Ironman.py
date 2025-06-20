27ce1097e4cb8efd7202ebaa386259a713982e31
CarlosJGarcia <cgarciams@hotmail.com>
2025-06-20 16:19:37 +0200

    Textos dias 3 y 4

--- Mantra_Ironman.py
+++ Mantra_Ironman.py
@@ -2,12 +2,14 @@
 # Pulsar F5 para ejecutar el código en IDLE
 # Pulsar Ctrl+F5 para ejecutar el código en VSCode
 # En macOS, para que funcione F5, pulsar Fn (globo terráqueo) + F5
+# $ git add Mantra_Ironman.py
 # $ git commit -m "Comentario"
 # $ git push
 
-mantra = ["Confío en mí. He entrenado lo mejor que he podido y domino la distancia, por ello nadaré, pedalearé y correré para mí."]
+mantra = ["Confío en mí. He entrenado lo mejor que he podido y domino la distancia, por ello nadaré, pedalearé y correré para mí mismo."]
 mantra.append("Los demás compiten conmigo, pero no son mis adversarios. Compito para mí.")
-mantra.append("Debo ver el triatlón como algo divertido. Alegría y diversión pese a la dificultad. Porque tengo el privilegio de deslizarme.")
+mantra.append("El triatlón debo verlo como algo divertido. Alegría y diversión pese a la dificultad, porque tengo el privilegio de deslizarme en el agua, viajar en bici y superar una larga distancia corriendo. Soy un privilegiado por ello.")
+mantra.append("No tengo miedo pero las distancias me imponen respeto. Es natural.")
 
 print("\nMantra del Ironman")
 n = 1
@@ -15,4 +17,3 @@ for item in mantra:
     print(f"{n}: {item}")
     n += 1
 print()
-print("Fin")
