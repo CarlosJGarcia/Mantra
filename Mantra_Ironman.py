d67008afebcf854d84c7e67b958cf8872df3590b
CarlosJGarcia <cgarciams@hotmail.com>
2025-06-26 09:15:52 +0200

    Actualizado al punto 10

--- Mantra_Ironman.py
+++ Mantra_Ironman.py
@@ -14,6 +14,8 @@ mantra.append("Esta prueba es mi prueba, porque me ofrece la posibilidad de demo
 mantra.append("No me importa en absoluto lo que hacen los demás. Me concentro en mí y en mi esfuerto.")
 mantra.append("Me oriento en mi cuerpo, mi mente, mis datos, mis sensaciones.")
 mantra.append("La tensión interna me sirve para motivarme para la competición y me ayuda en el rendimiento deportivo a desarrollar.")
+mantra.append("Son horas que vivo con alegría, porque son genuinamente mías y no las comparto con nadie. Es mi vivencia personal, genuina, intransferible y quiero disfrutar de ella.")
+mantra.append("El objetivo más importante es llegar a la meta, la planificación está en un segundo lugar y el objetivo de la planificación es alcanzar un mejor rendimiento.")
 
 print("\nMantra del Ironman")
 n = 1
